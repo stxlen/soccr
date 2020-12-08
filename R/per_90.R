621e468a11aaa60f3c33189b8ce363712d7c666c
Sean Steele <deletemess@icloud.com>
2020-12-07 17:25:09 -0800

    added documentation

--- R/per_90.R
+++ R/per_90.R
@@ -1,4 +1,9 @@
-# converts a stat to a per 90 metric
+#' Per 90 Converstion
+#'
+#' This function converts a raw stat to a per 90 metric
+#'
+#' @param stat The stat to convert to a per 90 metric
+#' @param minutes_played The total number of minutes played
 
 per_90 <- function(stat, minutes_played){
   round(stat / minutes_played * 90, digits = 2)
