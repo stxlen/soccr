0878366cbe91de0fd8277aa1e9a5ec80ee1c205a
stxlen <deletemess@icloud.com>
2023-12-20 22:51:12 -0800

    Added param details. Updated to reflect name change in sub-scripts.

--- R/xg_simulate_match.R
+++ R/xg_simulate_match.R
@@ -1,17 +1,17 @@
 #' Simulate the number of goals scored given a vector of shot xG values for two teams
 #'
-#' @param team_a_shots_xg
-#' @param team_b_shots_xg
+#' @param team_a_shots_xg A list containing the xG values for each shot taken by team A
+#' @param team_b_shots_xg A list containing the xG values for each shot taken by team B
 #'
 #' @return
 #' @export
 #'
 #' @examples
-xg_simulate_match <- function(team_a_shots_xg, team_b_shots_xg){
+xg_sim_match <- function(team_a_shots_xg, team_b_shots_xg){
 
   # Simulate goals for each team
-  team_a_goals <- xg_simulate_goal(team_a_shots_xg)
-  team_b_goals <- xg_simulate_goal(team_b_shots_xg)
+  team_a_goals <- xg_sim_goal(unlist(team_a_shots_xg))
+  team_b_goals <- xg_sim_goal(unlist(team_b_shots_xg))
 
   # Return the goals as separate lists
   return(list(team_a_goals, team_b_goals))
