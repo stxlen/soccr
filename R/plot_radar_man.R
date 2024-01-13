359a4e5747c8d23b0fd706c407c05a5a3552df93
stxlen <deletemess@icloud.com>
2024-01-12 17:12:42 -0800

    Added option to add a logo

--- R/plot_radar_man.R
+++ R/plot_radar_man.R
@@ -7,8 +7,9 @@
 #' @param subtitle Text to appear in the subtile.
 #' @param caption Text to appear in the caption (bottom)
 #' @param theme_color The theme color to use: light or dark. Default is light.
-#' @param pos_group The position group to use for the average line text. Not required
-
+#' @param pos_group The position group to use for the average line text. Not required.
+#' @param logo_path The path to the logo to use. Not required.
+#' @param title_loc The location of the title: left, center, or right. Default is left.
 #'
 #' @return A ggplot object
 #' @export
@@ -32,7 +33,7 @@
 #'                     theme_color = "light")
 #' p
 
-plot_radar_man <- function(df, title, subtitle = "", caption = "", pos_group = "", theme_color){
+plot_radar_man <- function(df, title, subtitle = "", caption = "", pos_group = "", theme_color, logo_path = "", title_loc = "left"){
 
   # SET THEME COLORS ###########################################################################
   if (theme_color == "light" || theme_color == "") {
@@ -55,6 +56,20 @@ plot_radar_man <- function(df, title, subtitle = "", caption = "", pos_group = "
     colorLine <- "black"
   }
 
+  # SET LOGO DEFAULTS ###########################################################################
+
+  if (is.null(logo_path)){
+    title_pos <- 0
+  } else if (title_loc == "center"){
+    title_pos <- 0.5
+  } else if (title_loc == "right"){
+    title_pos <- 1
+  } else {
+    title_pos <- 0
+  }
+
+  # PLOT ########################################################################################
+
   player_data <- df |>
     mutate(statistic = factor(statistic,
                               levels = df |> pull(statistic) |> unique()
@@ -69,40 +84,55 @@ plot_radar_man <- function(df, title, subtitle = "", caption = "", pos_group = "
     distinct(category) |>
     tibble::deframe()
 
-  ggplot(player_data, aes(statistic, percentile)) +
-    geom_bar(aes(y = 100, fill = category), stat = "identity", width = 1, color = fill_b,
-             alpha = 0.1, show.legend = FALSE) +
-    geom_bar(stat = "identity", width = 1, aes(fill = category), color = fill_b, alpha = 1) +
-    coord_polar(clip = "off") +
-    geom_hline(yintercept = 25, color = colorLine, linetype = "dashed", alpha = 0.8) +
-    geom_hline(yintercept = 50, color = colorLine, linetype = "dashed", alpha = 0.8) +
-    geom_hline(yintercept = 75, color = colorLine, linetype = "dashed", alpha = 0.8) +
-    scale_fill_manual(values = colors_map, breaks = legend_map) +
-    geom_label(aes(y = 90, label = value, fill = category), size = 3, color = fill_b, show.legend = FALSE) +
-    geom_text(x = 0.5, y = 50, label = paste0("Avg ", pos_group), vjust = -0.5, alpha = 0.05, size = 3) +  # Place text at the first bar
-    scale_y_continuous(limits = c(-20, 100)) +
-    labs(fill = "",
-         caption = caption,
-         title = title,
-         subtitle = subtitle) +
-    theme_minimal() +
-    theme(plot.background = element_rect(fill = fill_b, color = color_b),
-          panel.background = element_rect(fill = fill_b, color = color_b),
-          legend.position = "bottom",
-          legend.text = element_text(size = 9, color = colorText),
-          legend.key.size = unit(0.5, "cm"),
-          legend.spacing.x = unit(1, "mm"),  # Adjust spacing as needed
-          axis.title.y = element_blank(),
-          axis.title.x = element_blank(),
-          axis.text.y = element_blank(),
-          axis.text.x = element_text(size = 12, color = colorText),
-          text = element_text(color = colorText, size = 20),
-          plot.title = element_text(hjust = 0.0, size = 26, color = colorText, face = "bold"),
-          plot.subtitle = element_text(hjust = 0.0, size = 12, color = colorText),
-          plot.caption = element_text(hjust = 0.5, size = 9, color = colorText),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank()) +
-    scale_x_discrete(labels = function(x) stringr::str_wrap(x, width = 1))
+  p <- ggplot(player_data, aes(statistic, percentile)) +
+       geom_bar(aes(y = 100, fill = category), stat = "identity", width = 1, color = fill_b,
+                alpha = 0.1, show.legend = FALSE) +
+       geom_bar(stat = "identity", width = 1, aes(fill = category), color = fill_b, alpha = 1) +
+       coord_polar(clip = "off") +
+       geom_hline(yintercept = 25, color = colorLine, linetype = "dashed", alpha = 0.8) +
+       geom_hline(yintercept = 50, color = colorLine, linetype = "dashed", alpha = 0.8) +
+       geom_hline(yintercept = 75, color = colorLine, linetype = "dashed", alpha = 0.8) +
+       scale_fill_manual(values = colors_map, breaks = legend_map) +
+       geom_label(aes(y = 90, label = value, fill = category), size = 3, color = fill_b, show.legend = FALSE) +
+       geom_text(x = 0.5, y = 50, label = paste0("Avg ", pos_group), vjust = -0.5, alpha = 0.05, size = 3) +  # Place text at the first bar
+       scale_y_continuous(limits = c(-20, 100)) +
+       labs(fill = "",
+            caption = caption,
+            title = title,
+            subtitle = subtitle) +
+       theme_minimal() +
+       theme(plot.background = element_rect(fill = fill_b, color = color_b),
+             panel.background = element_rect(fill = fill_b, color = color_b),
+             legend.position = "bottom",
+             legend.text = element_text(size = 9, color = colorText),
+             legend.key.size = unit(0.5, "cm"),
+             legend.spacing.x = unit(1, "mm"),  # Adjust spacing as needed
+             axis.title.y = element_blank(),
+             axis.title.x = element_blank(),
+             axis.text.y = element_blank(),
+             axis.text.x = element_text(size = 12, color = colorText),
+             text = element_text(color = colorText, size = 20),
+             plot.title = element_text(hjust = title_pos, size = 26, color = colorText, face = "bold"),
+             plot.subtitle = element_text(hjust = title_pos, size = 12, color = colorText),
+             plot.caption = element_text(hjust = 0.5, size = 9, color = colorText),
+             panel.grid.major = element_blank(),
+             panel.grid.minor = element_blank()) +
+       scale_x_discrete(labels = function(x) stringr::str_wrap(x, width = 1))
+
+
+
+  # RETURN WITH OR WITHOUT A LOGO ###########################################################################
+
+  if (logo_path == "") {
+
+    return(p)
+
+  } else {
+
+    temp_plot <- tempfile("plot", fileext = ".png")
+    ggsave(temp_plot, p, width = 3000, height =2800, units = "px")
+    plot_add_logo(temp_plot, logo_path, logo_position = "top right", logo_scale = 12, logo_padding = 0.01)
+  }
 
 }
 
